3095c31980640bfb93cafb2193b7dcc0be4b9967
Ryan Burn <ryan.burn@gmail.com>
2017-12-19 17:59:52 -0800

    Print the current time.

--- example/helloworld.py
+++ example/helloworld.py
@@ -1,6 +1,7 @@
+from datetime import datetime
 from flask import Flask
 app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Hello World!"
+    return "Hello, the time is %s\n" % str(datetime.now())
