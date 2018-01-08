c2793a39dee7d2dc02cf5cc564cd91fae34d14ae
Ryan Burn <ryan.burn@gmail.com>
2018-01-08 15:53:27 +0000

    Switch to use v2 config.

--- example/start_front.sh
+++ example/start_front.sh
@@ -1,3 +1,3 @@
 export JAEGER_AGENT_HOST=`getent hosts jaeger | awk '{ print $1 }'`
 envsubst </app/envoy-jaeger.yaml >/etc/envoy-jaeger.yaml
-envoy -c front-envoy.json --service-cluster front-proxy 
+envoy -c /app/front-envoy.yaml --service-cluster front-proxy --v2-config-only
